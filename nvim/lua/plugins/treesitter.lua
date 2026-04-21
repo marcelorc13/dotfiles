0a512febff3ed4f61ab1eda24ac6d9883cf6e0ef
Marcelo Ramalho <marceloramalhocdev@gmail.com>
2026-04-21 11:44:14 -0300

    nvim: fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-	branch = 'master',
+    branch = "main",
     build = ":TSUpdate",
-	main = 'nvim-treesitter.config',
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = {
-          "javascript", "typescript", "tsx", "html", "css", "json",
-          "go", "gomod", "gowork", "gosum",
-          "lua", "vim", "vimdoc", "templ", "python", "sql", "markdown", "markdown_inline"
-        },
-		auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        autotag = {
-          enable = true,
-        },
-      }
-    end,
+    main = "nvim-treesitter.config",
+    opts = {
+      ensure_installed = {
+        "javascript", "typescript", "tsx", "html", "css", "json",
+        "go", "gomod", "gowork", "gosum",
+        "lua", "vim", "vimdoc", "templ", "python", "sql", "markdown", "markdown_inline",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 }
