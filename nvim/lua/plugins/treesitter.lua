cf66c7110e62b896def2c61261827367d359f184
Marcelo Ramalho <marceloramalhocdev@gmail.com>
2026-06-05 23:59:55 -0300

    nvim: fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,8 +11,20 @@ return {
         "lua", "vim", "vimdoc", "templ", "python", "sql", "markdown", "markdown_inline", "razor",
       },
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.config").setup(opts)
+      -- new main branch does not auto-enable features; must wire up manually
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          local ok = pcall(vim.treesitter.start, ev.buf)
+          if ok then
+            vim.wo[0][0].foldmethod = "expr"
+            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo[0][0].foldlevel = 99
+          end
+        end,
+      })
+    end,
   },
 }
